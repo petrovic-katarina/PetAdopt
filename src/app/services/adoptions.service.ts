f53756b4a074206a8cfa1f915f48623e5dc1d081
Katarina Petrovic <katarinap35@gmail.com>
2023-10-27 16:05:32 +0200

    dodat delete zahtev za usvajanje ljubimca

--- src/app/services/adoptions.service.ts
+++ src/app/services/adoptions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Adoption } from '../model/adoption.model';
 import { HttpClient } from '@angular/common/http';
+import { AdoptionList } from '../model/adoption-list.model';
 
 const baseUrlAdoptions = 'http://localhost:3000/api/adoptions';
 
@@ -14,6 +15,20 @@ export class AdoptionsService {
 
   postAdoption(adoption: Adoption): Observable<Adoption> {
     return this.http.post(`${baseUrlAdoptions}`, adoption).pipe(map((data: any) => {
+      return new Adoption(data);
+    }))
+  }
+
+  showAllAdoptions(): Observable<AdoptionList> {
+    return this.http.get(`${baseUrlAdoptions}`).pipe(map((data: any) => {
+      return new AdoptionList(data)
+    }))
+  }
+
+
+  // http://localhost:3000/api/adoptions/:id
+  deleteAdoption(id: number): Observable<Adoption> {
+    return this.http.delete(`${baseUrlAdoptions}/${id}`).pipe(map((data: any) => {
       return new Adoption(data);
     }))
   }
